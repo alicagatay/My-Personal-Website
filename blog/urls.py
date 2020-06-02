b70e3e9a74d1f0f690f249f27c7792ec4fbafb01
alicagatay <aliccagatay@gmail.com>
2020-06-02 12:40:50 +0300

    Changed url naming for the blog.

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='Homepage'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('blogposts/post<int:pk>/', views.post_detail, name='post_detail'),
     path('blogposts', views.post_list, name='post_list')
 ]
