df21af5bea6dbd86e45bd97f8b329761500ac200
alicagatay <aliccagatay@gmail.com>
2020-06-03 11:57:14 +0300

    Some fixes with the design and urls.

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='Homepage'),
-    path('blogposts/post<int:pk>/', views.post_detail, name='post_detail'),
-    path('blogposts', views.post_list, name='post_list')
+    path('blog/post<int:pk>/', views.post_detail, name='post_detail'),
+    path('blog', views.post_list, name='post_list')
 ]
